052b15e43561884d1a1adf819a9007ddb8a80056
sbhagwatvnera <sbhagwat@vmware.com>
2019-07-16 12:27:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ from setuptools import setup
 setup(
     setup_requires=['pbr'],
     pbr=True,
-    include_package_data=True
+    include_package_data=True,
+    package_data={	
+        '': ['*.md'],	
+    },
 )
