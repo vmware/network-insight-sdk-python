4a554725e48c95528fdb8b4f3229195581f85076
Sandesh Bhagwat <sbhagwat@vmware.com>
2019-07-23 17:26:36 +0530

    Add script to add in application

--- examples/application_creation.py
+++ examples/application_creation.py
@@ -15,63 +15,37 @@ import logging
 logger = logging.getLogger("vrni_sdk")
 
 
-def create_appication_and_tiers(application_api, application_name):
-    app_name = "{}".format(application_name)
+def create_appication_and_tiers(application_api, args):
+    app_name = "{}".format(args.application_name)
     body = {"name": app_name}
     app = application_api.add_application(body)
     logger.info("Application {} created".format(app_name))
-    create_tiers(app_name, application_api, app)
+    create_tiers(app_name, application_api, app, args.vm_names)
 
 
-def create_tiers(app_name, application_api, app):
+def create_tiers(app_name, application_api, app, vm_names):
     # Create tier using swagger models
     tier1 = swagger_client.Tier()
     tier1.entity_type = swagger_client.AllEntityType.TIER
-    tier1.name = "%s_t1" % app_name
+    tier1.name = "%s_tier" % app_name
+    vms = vm_names.split(',')
+
+    filter = "name={}".format(vms[0])
+    if len(vms) > 1:
+        for vm in vms[1:]:
+            filter = filter+ "or name".format(vm)
 
     grp_membership_criteria = swagger_client.GroupMembershipCriteria()
     grp_membership_criteria.membership_type = "SearchMembershipCriteria"
     grp_membership_criteria.ip_address_membership_criteria = None
     grp_membership_criteria.search_membership_criteria = swagger_client.SearchMembershipCriteria(
         entity_type=swagger_client.AllEntityType.VIRTUALMACHINE,
-        filter="name='redmond-vm-01'")
+        filter=filter)
 
     tier1.group_membership_criteria = [grp_membership_criteria]
 
     application_api.add_tier(app.entity_id, tier1)
-    logger.info("Tier 1 created")
-
-    # create tier using json
-    tier_2 = {
-        "name": "%s_t2" % app_name,
-        "entity_type": "Tier",
-        "group_membership_criteria": [
-            {
-                "membership_type": "IPAddressMembershipCriteria",
-                "ip_address_membership_criteria": {
-                    "ip_addresses": ["192.168.1.3"]
-                }
-            }
-        ]
-    }
-    application_api.add_tier(app.entity_id, tier_2)
-    logger.info("Tier 2 created")
-
-    tier_3 = {
-        "name": "%s_t3" % app_name,
-        "entity_type": "Tier",
-        "group_membership_criteria": [
-            {
-                "membership_type": "IPAddressMembershipCriteria",
-                "ip_address_membership_criteria": {
-                    "ip_addresses": ["192.168.1.4"]
-                }
-            }
-        ]
-    }
-    application_api.add_tier(app.entity_id, tier_3)
-    logger.info("Tier 3 created")
-
+    logger.info("{}_tier created".format(app_name))
 
 def delete_application(application_api, application_name):
     apps = application_api.list_applications()
@@ -85,14 +59,15 @@ def delete_application(application_api, application_name):
             break
 
 
-def main():
+def main(args):
     # Create application API client object
     application_api = swagger_client.ApplicationsApi()
-    create_appication_and_tiers(application_api, "demo_app_1")
-    # delete_application(application_api, "demo_app_1")
+    create_appication_and_tiers(application_api, args)
 
 def parse_arguments():
     parser = init_api_client.parse_arguments()
+    parser.add_argument("--application_name",action="store", help="Provide Application name", required=True)
+    parser.add_argument("--vm_names", action="store", help="Provide comma separated names of vms", required=True)
     args = parser.parse_args()
     return args
 
@@ -100,4 +75,4 @@ if __name__ == '__main__':
     args = parse_arguments()
     utilities.configure_logging("/tmp")
     api_client = init_api_client.get_api_client(args)
-    main()
+    main(args)
