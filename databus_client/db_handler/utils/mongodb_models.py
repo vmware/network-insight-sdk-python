1a19b5f7e075ad52ddee06703d49f5b39d479246
Sonam Sinha <ssinha2@ssinha2PY3WW.vmware.com>
2023-10-04 17:30:25 +0530

    changes

--- databus_client/db_handler/utils/mongodb_models.py
+++ databus_client/db_handler/utils/mongodb_models.py
@@ -180,7 +180,7 @@ class DatabusClientNsxtEdgeNodeMetricsMessageGroupData(mongoengine.Document):
         ]
     }
 
-    class DatabusClientFlowsMetricsMessageGroupData(mongoengine.Document):
+class DatabusClientFlowsMetricsMessageGroupData(mongoengine.Document):
     source = mongoengine.StringField()  # setup platform customer_id
     entity_id = mongoengine.StringField()
     metric_name = mongoengine.StringField()
